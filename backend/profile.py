2c767a70e465a7bb99c44d7aca4b1d79c9b53c33
SnailRabbids <36141552+SnailRabbids@users.noreply.github.com>
2021-11-27 14:22:26 +0100

    Xkorni02 (#15)
    * added authorization
    
    * video array moved from user to spot

--- backend/profile.py
+++ backend/profile.py
@@ -1,6 +1,7 @@
 from flask_restful import Resource
 from flask import request
 
+import copy
 import db
 
 class Profile(Resource):
@@ -9,7 +10,16 @@ class Profile(Resource):
         actual_user = db.get_user(json_data["id"])
 
         for spot in actual_user["my_spots"]:
-            actual_user["my_spots_info"][spot] = db.get_spot(spot)
+            actual_user["my_spots_info"][spot] = copy.deepcopy(db.get_spot(spot))
+            temp_videos = []
+            for video in actual_user["my_spots_info"][spot]["videos"]:
+                if video["user_id"] == int(json_data["id"]):
+                    # print("need_to_add : " + str(video))
+                    temp_videos.append(video)
+            actual_user["my_spots_info"][spot]["videos"] = copy.deepcopy(temp_videos)
+        
+        # print(db.get_spot(0))
+        # print(actual_user["my_spots_info"][0])
         return actual_user
     
     def get(self):
@@ -19,28 +29,37 @@ class Profile(Resource):
 class EditProfile(Resource):
     def post(self):
         json_data = request.get_json(force=True)
-        actual_user = db.get_user(json_data["id"])
+        if json_data["id"] == "actual_user":
+            json_data["id"] = db.actual_user
+        actual_user = copy.deepcopy(db.get_user(json_data["id"]))
 
-        if json_data["pass"] != json_data["rep_pass"]:
+        if "pass" in json_data and "rep_pass" in json_data and json_data["pass"] != json_data["rep_pass"]:
             return {'ERROR': 'PASSWORD AND REPEAT PASSWORD ARE NOT SAME!'}
         
-        if json_data["pass"] != "":
+        if "pass" in json_data and json_data["pass"] != "":
             actual_user["pass"] = json_data["pass"]
 
-        if json_data["email"] != "":
+        if "email" in json_data and json_data["email"] != "":
             actual_user["email"] = json_data["email"]
 
-        if json_data["name"] != "":
+        if "name" in json_data and json_data["name"] != "":
             actual_user["name"] = json_data["name"]
 
-        if json_data["instagram"] != "":
+        if "instagram" in json_data and json_data["instagram"] != "":
             actual_user["instagram"] = json_data["instagram"]
         
-        if json_data["facebook"] != "":
+        if "facebook" in json_data and json_data["facebook"] != "":
             actual_user["facebook"] = json_data["facebook"]
         
-        if json_data["tiktok"] != "":
+        if "tiktok" in json_data and json_data["tiktok"] != "":
             actual_user["tiktok"] = json_data["tiktok"]
+
+        if "spot_id" in json_data:
+            print(json_data["spot_id"])
+            if int(json_data["spot_id"]) not in actual_user["my_spots"]:
+                actual_user["my_spots"].append(int(json_data["spot_id"]))
+            else:
+                actual_user["my_spots"].remove(int(json_data["spot_id"]))
 
         db.set_user(json_data["id"], actual_user)
 
